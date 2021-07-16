e8e3d1c2b092b413a6d7bd72c99352872c9e7e2c
kiyanskiy <kiyanskiy@ukr.net>
2021-07-16 13:31:51 +0300

    Initial Commit

---
+++ gitHomework14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitHomework14
+//
+//  Created by Oleksandr Kiianskyi on 16.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
