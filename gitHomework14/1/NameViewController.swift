ef9dab851f582775443269e3e04cbe5f1f17ed34
kiyanskiy <kiyanskiy@ukr.net>
2021-07-16 13:56:04 +0300

    todo list

---
+++ gitHomework14/1/NameViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NameViewController.swift
+//  homework14Git
+//
+//  Created by Oleksandr Kiianskyi on 16.07.2021.
+//
+import UIKit
+
+class NameViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var surnameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameTextField.text = Human.shared.name
+        surnameTextField.text = Human.shared.surname
+    }
+    @IBAction func didChangeName(_ sender: Any) {
+        Human.shared.name = nameTextField.text
+    }
+    
+    @IBAction func didChangeSurname(_ sender: Any) {
+        Human.shared.surname = surnameTextField.text
+    }
+}
---
+++ gitHomework14/Model/TaskCD+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TaskCD+CoreDataClass.swift
+//  gitHomework14
+//
+//  Created by Oleksandr Kiianskyi on 16.07.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TaskCD)
+public class TaskCD: NSManagedObject {
+
+}
